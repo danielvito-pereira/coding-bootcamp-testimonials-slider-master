d9b2bc3318c59defcbbdf339059c9ae91316716e
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2021-04-30 16:13:10 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
 const prev = document.querySelector('.pev');
 const next = document.querySelector('.next');
 const clientQuote = document.querySelector('.client-quote')
-const img = document.querySelector('.img');
-const client = document.querySelector('.client-name');
-const occupation = document.querySelector('.client-ocupation');
+const imgHolder= document.querySelector('.img');
+const clientName  = document.querySelector('.client-name');
+const occupation = document.querySelector('.client-occupation');
 
 //altera informações dos clientes
 const clients = [
     {
         name: 'Tanya Sinclair',
         occupation: 'UX Engineer',
-        src: './assets/image-tanya.jpg',
+        src: 'images/image-tanya.jpg',
         quote: `“ I’ve been interested in coding for a while but never taken the jump, until now. 
         I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
         excited about the future. ”`
@@ -18,7 +18,7 @@ const clients = [
     {
         name: 'John Tarkpor',
         occupation: 'Junior Front-end Developer',
-        src: './assets/image-john.jpg',
+        src: 'images/image-john.jpg',
         quote: `“ If you want to lay the best foundation possible I’d recommend taking this course. 
         The depth the instructors go into is incredible. I now feel so confident about 
         starting up as a professional developer. ”`
@@ -28,7 +28,7 @@ const clients = [
 //functions
 
 function prevClient() {
-    let client = 0;
+    let clientIndex = 0;
     let currentClientName = clientName.texContent;
 
     clientQuote.textContent = clients[clientIndex].quote;
